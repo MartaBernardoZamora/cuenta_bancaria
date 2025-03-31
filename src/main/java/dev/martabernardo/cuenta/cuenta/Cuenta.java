4a3ce8508a2f57a4b0381d1ab369cc7a7c552eb1
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-31 21:12:41 +0200

    STATUS OK testConsignar from CuentaTest

---
+++ src/main/java/dev/martabernardo/cuenta/cuenta/Cuenta.java
@@ -0,0 +1,27 @@
+package dev.martabernardo.cuenta.cuenta;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "cuenta")
+public class Cuenta {
+
+    protected float saldo;
+
+    public Cuenta(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public void consignar(float consigna) {
+        saldo += consigna;
+    }
+
+
+}
